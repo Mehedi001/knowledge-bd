5d1bf17fde40a6a1a9f32604b27428eb16f51183
Mehedi001 <mh.hridoy5070@gmail.com>
2023-03-31 01:04:57 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h3>hi</h3>
+        </div>
+    );
+};
+
+export default Header;
